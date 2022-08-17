54d274b1b2f88236fa091731366d6383f7a24880
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2022-08-17 19:16:50 +0800

    Update check.f90

--- test/check.f90
+++ test/check.f90
@@ -1,11 +1,35 @@
-program check
+ program check
    use fortran_vector
    implicit none
    type(vector_int)::a
+   integer,allocatable::x(:)
    integer::i
+   integer,parameter::n=100000
+   integer(8)::tic,toc,rate
+   !----------------
+   call system_clock(tic,rate)
    call a%init()
-   call a%append([1,1,2,2,333,33,33,44,55,66,77,88,9,10,10])
-   call a%write()
-   call a%unique()
-   call a%write()
+   do i=1,n
+      call a%append(i)
+   end do
+   call a%cut()
+   call system_clock(toc,rate)
+   write(*,*)"vector int",real(toc-tic,8)/rate,a%get(n)
+   !----------------
+   call system_clock(tic,rate)
+   allocate(x(0))
+   do i=1,n
+      x=[x,i]
+   end do
+   call system_clock(toc,rate)
+   write(*,*)"append",real(toc-tic,8)/rate,x(n)
+   !----------------
+   deallocate(x)
+   call system_clock(tic,rate)
+   allocate(x(n))
+   do i=1,n
+      x(i)=i
+   end do
+   call system_clock(toc,rate)
+   write(*,*)"array",real(toc-tic,8)/rate,x(n)
 end program check
