72b520776bd48eb987caa48eea13723aa6a03dd4
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2022-08-17 18:42:34 +0800

    Create check.f90

---
+++ test/check.f90
@@ -0,0 +1,11 @@
+program check
+   use fortran_vector
+   implicit none
+   type(vector_int)::a
+   integer::i
+   call a%init()
+   call a%append([1,1,2,2,333,33,33,44,55,66,77,88,9,10,10])
+   call a%write()
+   call a%unique()
+   call a%write()
+end program check
